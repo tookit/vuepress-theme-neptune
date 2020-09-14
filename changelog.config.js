ea815f0a8fc2aca09bb875ec1d949b5f548d209b
michaelwang <wangqiangshen@gmail.com>
2020-09-14 20:44:31 +0800

    Feature test

---
+++ changelog.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  fileName: 'CHANGELOG.md',
+
+  dateFormat: 'dd/LL/yyyy',
+
+  separator: '-',
+
+  triggers: ['🐛 Bug', '🚀 Feature', '💥 Change'],
+
+  pluraliseTrigger: true,
+
+  skipEmptyCommitMessages: false,
+
+  customMessageFormatter: null,
+
+  customHeadingFormatter: null
+}
