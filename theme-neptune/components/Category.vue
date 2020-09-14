4fafc0230180d5f4eb6b1a947277479a77a18471
michaelwang <wangqiangshen@gmail.com>
2020-09-14 11:43:20 +0800

    update category and article page

--- theme-neptune/components/Category.vue
+++ theme-neptune/components/Category.vue
@@ -4,45 +4,67 @@
       <v-toolbar dark tag="div" tile flat color="grey darken-4">
         <v-toolbar-title>{{ $page.title }}</v-toolbar-title>
         <v-spacer />
-        <v-btn icon>
-          <v-icon>mdi-arrow-left</v-icon>
-        </v-btn>
+        <v-btn-toggle
+          v-model="viewMode"
+          tile
+          dense
+          color="deep-purple accent-3"
+        >
+          <v-btn tile icon value="grid">
+            <v-icon>mdi-view-grid</v-icon>
+          </v-btn>
+          <v-btn tile icon value="list">
+            <v-icon>mdi-view-list</v-icon>
+          </v-btn>
+        </v-btn-toggle>
       </v-toolbar>
     </v-sheet>
     <v-container>
       <v-row>
-        <v-col cols="12">
-          <v-list three-line>
-            <template v-for="item in posts">
-              <v-list-item :key="item.key" :to="item.path">
-                <v-list-item-avatar tile color="grey" size="80">
-                </v-list-item-avatar>
-                <v-list-item-content>
-                  <v-list-item-title>
-                    {{ item.title }}
-                  </v-list-item-title>
-                  <v-list-item-subtitle
-                    >Greyhound divisely hello coldly
-                    fonwderfully</v-list-item-subtitle
-                  >
-                </v-list-item-content>
-              </v-list-item>
-            </template>
-          </v-list>
-        </v-col>
-        <!-- <v-col
-            cols="6"
+        <template v-if="viewMode === 'list'">
+          <v-col cols="12">
+            <v-list three-line>
+              <template v-for="item in posts">
+                <v-list-item :key="item.key" :to="item.path">
+                  <v-list-item-avatar tile color="grey" size="80">
+                  </v-list-item-avatar>
+                  <v-list-item-content>
+                    <v-list-item-title>
+                      {{ item.title }}
+                    </v-list-item-title>
+                    <v-list-item-subtitle>
+                      {{ item.frontmatter.date }}
+                    </v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+              </template>
+            </v-list>
+          </v-col>
+        </template>
+        <template v-else>
+          <v-col
+            cols="4"
             v-if="posts.length"
             v-for="post in posts"
             :key="post.key"
           >
             <v-card ripple :to="post.path" tile>
-              <v-img
-                v-show="post.frontmatter.image"
-                :src="post.frontmatter.image"
-              />
-              <v-card-title>{{ post.title }}</v-card-title>
-            </v-card> -->
+              <v-img :src="computePostImage(post.frontmatter)" />
+              <v-card-text>
+                <v-list-item>
+                  <v-list-item-content>
+                    <v-list-item-title>
+                      {{ post.title }}
+                    </v-list-item-title>
+                    <v-list-item-subtitle>
+                      {{ post.frontmatter.date }}
+                    </v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </template>
       </v-row>
     </v-container>
   </v-main>
@@ -71,6 +93,10 @@ export default {
   methods: {
     handleToggleSideBar() {
       this.showDrawer = !this.showDrawer
+    },
+    computePostImage(frontmatter) {
+      console.log('here')
+      return frontmatter.image || '/img/mock.png'
     }
   },
   mounted() {},
